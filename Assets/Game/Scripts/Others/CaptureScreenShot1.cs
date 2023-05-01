ec3a33ff7f0c5c18b017a50fff4724d90a078706
pareshgiri1 <pareshgiri.aparanathi@yudiz.com>
2023-05-01 10:09:10 +0530

    .

---
+++ Assets/Game/Scripts/Others/CaptureScreenShot1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptureScreenShot1 : MonoBehaviour
+{
+
+
+    int i = 0;
+    private void Awake()
+    {
+        if (PlayerPrefs.HasKey("CaptureSnap"))
+        {
+            i = PlayerPrefs.GetInt("CaptureSnap");
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.C))
+        {
+            i++;
+            ScreenCapture.CaptureScreenshot(Application.dataPath + "/Screens/1242_2208/Sceen_" + i + ".png");
+        }
+    }
+
+    private void OnDisable()
+    {
+        PlayerPrefs.SetInt("CaptureSnap",i);
+        PlayerPrefs.Save();
+    }
+}
